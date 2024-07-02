0fdbb64c49df96677fb3348c38ebb1dd83839bf1
sanket-bhiwapurkar <sanket24bhiwapurkar@gmail.com>
2024-07-02 17:47:36 +0530

    state and conditional @if

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // State
+  counter: number = 0
+  isFive: boolean = false
 
+  // Methods
+  decrement() {
+    this.counter === 0 ? 0 : this.counter--
+    this.setIsFive()
+  }
+  increment() {
+    this.counter === 10 ? 10 : this.counter++
+    this.setIsFive()
+  }
+  setIsFive() {
+    this.isFive = this.counter === 5
+  }
 }
